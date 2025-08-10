bb46afada4f5e2b3697edcf05ac0c2e0518ea5ad
Brandon Hill <brandonjon26@gmail.com>
2025-08-10 18:28:45 -0500

    replaced hardcoded products with the .map function; added filter

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,4 +1,4 @@
-export function ProductCard({ product, background = "slategray", onPurchase, ...restProps }) {
+export function ProductCard({ product, background = "slategray", onPurchase }) {
 
   return (
     <article 
@@ -15,13 +15,14 @@ export function ProductCard({ product, background = "slategray", onPurchase, ...
       <img
         src={product.imageSrc}
         alt={product.title}
-        {...restProps}
+        width={128}
+        height={128}
       />
       <p>Specification:</p>
       <ul style={{ listStyle: "none", padding: 0 }}>
-        <li>{product.specification[0]}</li>
-        <li>{product.specification[1]}</li>
-        <li>{product.specification[2]}</li>
+        {product.specification.map((spec, index) => (
+          <li key={index}>{spec}</li>
+        ))}
       </ul>
       <button onClick={() => onPurchase(product)}>
         Buy (From ${product.price})
