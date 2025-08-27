b43f912dfeff7ef8a5c4b1f2a25c1d32a27410aa
Brandon Hill <brandonjon26@gmail.com>
2025-08-26 22:31:21 -0500

    useState hook

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import styles from "./ProductCard.module.css";
 
 export function ProductCard({ product, background = "slategray", onPurchase }) {
+  const [stockCount, setStockCount] = useState(product.stockCount);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleClick() {
+    setStockCount(stockCount - 1);
+    onPurchase(product);
+  }
+
   return (
     <article className={styles.Container} style={{ background }}>
       <h2>{product.title}</h2>
@@ -10,31 +19,35 @@ export function ProductCard({ product, background = "slategray", onPurchase }) {
         width={128}
         height={128}
       />
-      <p>Specification:</p>
-      <ul className={styles.Specification}>
-        {product.specification.map((spec, index) => (
-          <li key={index}>{spec}</li>
-        ))}
-      </ul>
-      <Status stockCount={product.stockCount} />
-      {product.stockCount > 0 && (
-        <button onClick={() => onPurchase(product)}>
-        Buy (From ${product.price})
-      </button>
+      <p>
+        Specification:{" "}
+        <button onClick={() => setShowMore(!showMore)}>
+          {showMore ? "hide" : "show"}
+        </button>
+      </p>
+      {showMore && (
+        <ul className={styles.Specification}>
+          {product.specification.map((spec, index) => (
+            <li key={index}>{spec}</li>
+          ))}
+        </ul>
+      )}
+      <Status stockCount={stockCount} />
+      {stockCount > 0 && (
+        <button onClick={handleClick}>Buy (From ${product.price})</button>
       )}      
     </article>
   );
 }
 
-function Status({ stockCount }) {
-  
+function Status({ stockCount }) {  
   const notAvailableTemplate = (
     <b><p className={styles.NotAvailableStatus}>Not available</p></b>
-  )
+  );
 
   const availableTemplate = (
     <b><p className={styles.AvailableStatus}>{stockCount} left in stock</p></b>
-  )
+  );
 
   return stockCount === 0 ? notAvailableTemplate : availableTemplate;
 }
